28adaff3ca7726d9d59c5b293136d4b63e34e3f1
Gabe Fierro <gtfierro225@gmail.com>
2011-09-22 18:21:25 -0700

    Fixed Loose Ends: attr_accessible for User model

--- db/migrate/20110912000004_add_photo_to_posts.rb
+++ db/migrate/20110912000004_add_photo_to_posts.rb
@@ -1,11 +1,11 @@
 class AddPhotoToPosts < ActiveRecord::Migration
-  def self.up
+  def self.down
     add_column :posts, :photo_file_name, :string
     add_column :posts, :photo_content_type, :string
     add_column :posts, :photo_file_size, :integer
   end
 
-  def self.down
+  def self.up
     remove_column :posts, :photo_file_size
     remove_column :posts, :photo_content_type
     remove_column :posts, :photo_file_name
