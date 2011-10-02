a119fa0f51db8dd6ae5f315fa4e7ff2dca817bf0
Kristine LOLWORTHY Chen <kristinechen@gmail.com>
2011-10-01 17:52:33 -0700

    Editing spacing between elements, changing database type for content

---
+++ db/migrate/20111002003257_change_posts_content_data_type.rb
@@ -0,0 +1,9 @@
+class ChangePostsContentDataType < ActiveRecord::Migration
+  def up
+    change_column :posts, :content, :text, :limit => nil
+  end
+
+  def down
+    change_column :posts, :content, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110922164301) do
+ActiveRecord::Schema.define(:version => 20111002003257) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
@@ -25,9 +25,12 @@ ActiveRecord::Schema.define(:version => 20110922164301) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
-    t.string   "content"
+    t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
   end
 
   create_table "signups", :force => true do |t|
@@ -41,6 +44,7 @@ ActiveRecord::Schema.define(:version => 20110922164301) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
+    t.string   "username"
     t.string   "password_hash"
     t.string   "password_salt"
     t.datetime "created_at"
