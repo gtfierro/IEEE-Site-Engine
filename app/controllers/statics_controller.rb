64dfaeb30019af87483f468765b323bfa02d3997
Kristine Chen <kristinechen@gmail.com>
2012-01-29 18:45:19 -0800

    Editing the officers list

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -7,7 +7,7 @@ class StaticsController < ApplicationController
     @execs = [['Tianbo Xu', 'Chair'], ['Pranava Adduri', 'External VP'], ['Mark Hettick', 'Internal VP'], ['Gabe Fierro', 'Secretary'], ['Jeff Tsui', 'Treasurer']]
     @activities = ['Leland Au', 'Alice Lee', 'Amanda Ren', 'Annie Shin', 'Edward Wu']
     @hope = ['Sean Soleyman', 'Oliver Zhu', 'Andrew Wun']
-    @indrel = ['Mihir Joshi', 'Benjamin Lui', 'Noel Moldvai', 'Mano Pagalavan', 'Alok Rai', 'Amudhan Venkatesan']
+    @indrel = ['Mihir Joshi', 'Benjamin Lui', 'Noel Moldvai', 'Amudhan Venkatesan', 'Mano Pagalavan']
     @io = ['Jaclyn Louie', 'Navin Eluthesen', 'Ayushi Samaddar', 'Jessika Wu', 'Muller Zhang']
     @prodev = ['Owen Lin', 'Deirdre Chen', 'Aime Ngongang', 'Jeremy Williams']
     @techops = ['Felix Wong', 'Guillaume Bellegarda', 'Sergio Hidalgo', 'Jonathan Hsu', 'William Ku', 'Sam Whitlock']
